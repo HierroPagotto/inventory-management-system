c57b43b8e3125d3335eafdaca9934a8f2f5cb1b5
HierroPagotto <hierro.reino@gmail.com>
2025-06-09 18:33:39 -0300

    fix: product deletion and duplicate movement issues

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,18 +1,7 @@
 class Produto < ApplicationRecord
-  has_many :movimentacoes, class_name: 'Movimentacao', dependent: :destroy
+  has_many :movimentacoes, dependent: :destroy
   
   validates :nome, presence: true
   validates :quantidade_estoque, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :preco, numericality: { greater_than: 0 }, allow_blank: true
-  
-  def atualizar_estoque!(tipo, quantidade)
-    case tipo
-    when 'entrada'
-      self.quantidade_estoque += quantidade
-    when 'saida'
-      raise 'Estoque insuficiente' if self.quantidade_estoque < quantidade
-      self.quantidade_estoque -= quantidade
-    end
-    save!
-  end
 end
